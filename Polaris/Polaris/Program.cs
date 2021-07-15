0388203ab1b9a98aa09473423973541ebc5befe1
Karrson Heumann <hkarrson@gmail.com>
2021-07-15 09:51:23 -0700

    Init

---
+++ Polaris/Polaris/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Polaris
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Hopefully soon I'll get the motivation to
+             * actually remake all of this.
+             * I've "iterated" way too much...
+             */
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
